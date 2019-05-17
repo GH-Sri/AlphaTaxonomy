aa8b8cae2c117443cf794c343333ae16e956f1e0
hwalbert <hwalbert@users.noreply.github.com>
2019-05-17 13:55:54 -0400

    Added argument to GetTenKs
    Argument allows selection of how many CIKs to use to get 10K data

--- CollectionOf10KScript.R
+++ CollectionOf10KScript.R
@@ -30,19 +30,6 @@ GetCompanyList <- function(Exchange = "NASDAQ"){
   
 }
 
-#Get the refreshed Company data from the NASDAQ website
-NASDAQ <- GetCompanyList(Exchange = "NASDAQ")
-
-load("/home/rstudio/RScripts/ALLCIK.RData")
-#read.csv(file = "C:/Users/hwalbert001/Documents/Company Stock Analysis/companylistFROMEUGENE.csv", colClasses = "character")
-NASDAQ <- merge(NASDAQ, ALLCIK, all.x = T, by = "Symbol")
-NASDAQ <- subset(NASDAQ, !is.na(NASDAQ$CIK))
-
-#Make a list of unique CIKs
-#note that all ciks are not unique
-CIKs <- as.numeric(unique(NASDAQ$CIK))
-CIKs <- CIKs[!is.na(CIKs)]
-
 #This function is originally from the edgar package...it has been adjusted to deal with the error:
 #"Error in writeLines(product.descr2, filename2) : object 'product.descr2' not found "
 newEDGAR <- function (cik.no, filing.year) 
@@ -166,15 +153,30 @@ newEDGAR <- function (cik.no, filing.year)
   return(output)
 }
 
+GetTenKs <- function(CIK_Index = 1:500){
+#Get the refreshed Company data from the NASDAQ website
+NASDAQ <- GetCompanyList(Exchange = "NASDAQ")
+
+load("/home/rstudio/RScripts/ALLCIK.RData")
+#read.csv(file = "C:/Users/hwalbert001/Documents/Company Stock Analysis/companylistFROMEUGENE.csv", colClasses = "character")
+NASDAQ <- merge(NASDAQ, ALLCIK, all.x = T, by = "Symbol")
+NASDAQ <- subset(NASDAQ, !is.na(NASDAQ$CIK))
+
+#Make a list of unique CIKs
+#note that all ciks are not unique
+CIKs <- as.numeric(unique(NASDAQ$CIK))
+CIKs <- CIKs[!is.na(CIKs)]
 
 #Okay, This is where we get all the Business Descriptions
 #The getBusinDescr() function pulls the master indexes from the SEC server for each year specified...
 #This gets stored in the working directory in a folder called Business description test
 counter = 0
-for(i in CIKs[1:500]){#THIS IS FOR TESTING
+# for(i in CIKs[1:500]){#THIS IS FOR TESTING
 # for(i in CIKs[1:length(CIKs)]){
+for(i in CIKs[CIK_Index]){
   counter <- counter + 1
   cat("Counter:", counter, "\n")
   cat("working on", i, "\n")
   mapply(newEDGAR, cik = i, filing.year = c(2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018))
+}
 }
