a8b04251af18cc368881458b8ab51a4db77f8506
hwalbert <hwalbert@users.noreply.github.com>
2019-05-19 11:17:35 -0400

    Consolidate 10Ks
    This function (CollectBDData()) looks for every Business Descriptions file downloaded and then reads it in a table. It then stacks the info from each into one table. Then it parses out the needed information into separate columns

--- CollectionOf10KScript.R
+++ CollectionOf10KScript.R
@@ -175,3 +175,30 @@ for(i in CIKs[1:500]){#THIS IS FOR TESTING
   cat("working on", i, "\n")
   mapply(newEDGAR, cik = i, filing.year = c(2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018))
 }
+
+
+
+#This function looks for every Business Descriptions file downloaded and then read it in a table
+#It then stacks the info from each into one table
+#Then it parses out the needed information into separate columns
+CollectBDData <- function(){
+  library(readtext)
+  finaldata <- data.frame()
+  #You will need to select where the folder with business description data is located
+  AllBDs <- list.files(choose.dir(caption = "Choose where folder with Business Description Data"), full.names = T)
+  for (i in AllBDs){
+    x <- readtext(i)
+    finaldata <- rbind(finaldata, x)
+  }
+  
+  breakoutInfo <- t(data.frame(strsplit(finaldata$doc_id, split = "_")))
+  finaldata <- cbind(finaldata, breakoutInfo)
+  names(finaldata) <- c("doc_id", "text", "CIK", "Source", "YearDate", "File")
+  finaldata <- merge(finaldata, NASDAQ, all.x = T, by = "CIK")
+  finaldata <- finaldata[c("CIK", "YearDate", "text", "Symbol", "Name.x")]
+  
+  # finaldata$text <- gsub("/(\r\n|\n|\r)/gm", "", finaldata$text)
+  finaldata$text <- gsub("[\r\n]", "", finaldata$text)
+  
+  return(finaldata)
+}
