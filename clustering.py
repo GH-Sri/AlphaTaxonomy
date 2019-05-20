43f092f92e15f95a25e9be5afe9bbfe0c9b8fc0c
eugenechoGH <44753441+eugenechoGH@users.noreply.github.com>
2019-05-20 18:03:50 -0400

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -13,6 +13,7 @@ import numpy as np
 import pandas as pd
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster, maxdists,ward
 from scipy.spatial.distance import pdist
+from sklearn.metrics.pairwise import cosine_similarity
 import csv
 import os
 #import matplotlib as plt
@@ -24,7 +25,7 @@ print('Creating Data')
 
 data = []
 labelList=[]
-file_dir = cwd + "\\Documents\\GitHub\\r-libraries\\"
+file_dir = cwd + "/Documents/GitHub/r-libraries/"
 #file_dir = cwd
 with open(file_dir+"cikVectorsExample1.csv", 'r') as csvfile:
     reader = csv.reader(csvfile)
@@ -37,7 +38,8 @@ print('Finding distances between nodes')
 
 dist = pdist(data)
 linked = ward(dist)
-#plt.figure(figsize=(35, 50)) 
+
+#plt.figure(figsize=(60, 100)) 
 #dendrogram(linked,  
 #            orientation='right',
 #            leaf_font_size=12.,
@@ -95,8 +97,8 @@ df_doc2vec['industry']=df_doc2vec['industry'].astype('category')
 df_sector_avg=df_doc2vec.groupby('sector').mean()
 df_industry_avg=df_doc2vec.groupby('industry').mean()
 
-df_sector_avg.to_csv('sector_avg.csv')
-df_industry_avg.to_csv('industry_avg.csv')
+df_sector_avg.to_csv(file_dir+'sector_avg.csv')
+df_industry_avg.to_csv(file_dir+'industry_avg.csv')
 
 
 
@@ -110,7 +112,8 @@ for i in range(0,len(arr_val)):
     a1=[]
     a1.append(labelList[i])
     for j in range(0,df_sector_avg.shape[0]):
-        a1.append(np.linalg.norm(arr_val[i]-df_sector_avg.values[j]))
+        a1.append(cosine_similarity(arr_val[i].reshape(1,-1),
+                                    df_sector_avg.values[j].reshape(1,-1))[0][0])
     arr_doc_dist.append(a1)
 
 df_doc_dist_sector=pd.DataFrame(arr_doc_dist)
@@ -128,7 +131,8 @@ for i in range(0,len(arr_val)):
     a1=[]
     a1.append(labelList[i])
     for j in range(0,df_industry_avg.shape[0]):
-        a1.append(np.linalg.norm(arr_val[i]-df_industry_avg.values[j]))
+        a1.append(cosine_similarity(arr_val[i].reshape(1,-1),
+                                    df_industry_avg.values[j].reshape(1,-1))[0][0])
     arr_doc_dist.append(a1)
 df_doc_dist_industry=pd.DataFrame(arr_doc_dist)
 arr_colnames=['cik-year']
@@ -136,9 +140,8 @@ for i in range(1,df_doc_dist_industry.shape[1]):
     arr_colnames.append('Industry '+str(i))
 df_doc_dist_industry.columns = arr_colnames
 
-df_doc_dist_sector.to_csv('doc_dist_sector.csv')
-df_doc_dist_industry.to_csv('doc_dist_industry.csv')
-
+df_doc_dist_sector.to_csv(file_dir+'doc_cossim_sector.csv')
+df_doc_dist_industry.to_csv(file_dir+'doc_cossim_industry.csv')
 
 
 
