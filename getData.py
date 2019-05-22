ebeb02aa6ab2cf70e5d79592bec5922af3d35c74
eugenechoGH <44753441+eugenechoGH@users.noreply.github.com>
2019-05-22 14:47:29 -0400

    Update getData.py

--- getData.py
+++ getData.py
@@ -1,16 +1,14 @@
+import boto
 from boto.s3.key import Key
 
 keyId = 'AKIAVO5KNXW5MLTJ2JOS'
-sKeyId='xUJmbLt+sRYTAcKJmsQE3D2r5Z2K1NbYBOit5lHX'
-srcFileName='BDData.csv'
+sKeyID='xUJmbLt+sRYTAcKJmsQE3D2r5Z2K1NbYBOit5lHX'
+srcFileName='10K-Raw-Data/BDData.csv'
 destFileName='data.csv'
-bucketName='gh-mdas-data-1'
+bucketName='at-mdas-data'
 
 conn=boto.connect_s3(keyId,sKeyID)
+#conn.get_all_buckets()
 bucket=conn.get_bucket(bucketName)
-
 k=Key(bucket,srcFileName)
-
 k.get_contents_to_filename(destFileName)
-
-
