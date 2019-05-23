e79d587becaeab11f817b48b013a94e5eb76c9c5
Owen Davies <odavies@peraton.com>
2019-05-23 05:20:33 +0000

    New RESTful endpoint for the entire company list to drive the big data grid

---
+++ lambda-json-rest/functions/get-companylist/lambda_function.py
@@ -0,0 +1,43 @@
+import sys
+import logging
+import rds_config
+import psycopg2
+from urllib.parse import unquote
+from psycopg2.extras import RealDictCursor
+import json
+
+# rds settings
+rds_host  = "mdas.c33zx3vjrof0.us-east-1.rds.amazonaws.com"
+name = rds_config.db_username
+password = rds_config.db_password
+db_name = rds_config.db_name
+
+# logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# connect using creds from rds_config.py
+try:
+    conn = psycopg2.connect( host=rds_host,user=name,password=password,dbname=db_name)
+except:
+    logger.error("ERROR: Unexpected error: Could not connect to PostgreSQL instance.")
+    sys.exit()
+logger.info("SUCCESS: Connection to RDS PostgreSQL instance succeeded")
+
+# SQL to get what this function is responsible for returning
+template = '''
+SELECT *
+FROM Company
+'''
+
+# executes upon API event
+def lambda_handler(event, context):
+    logger.info("Getting full company list")
+    with conn.cursor(cursor_factory=RealDictCursor) as cur:
+        cur.execute('SELECT * FROM Company')
+        conn.commit()
+        return {
+            'statusCode': 200,
+            'headers': { 'Content-Type': 'application/json' },
+            'body': json.dumps(cur.fetchall(), indent=2)
+        }
