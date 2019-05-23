2f4560208b1f56a4ff127339fcff8e961daf3530
Thomas Essig <tessig@harris.com>
2019-05-23 19:33:48 -0400

    missed a file

--- mdas-client/src/app/home/company-list.service.ts
+++ mdas-client/src/app/home/company-list.service.ts
@@ -11,40 +11,12 @@ export class CompanyListService {
   }
 
   getCompanyList() {
-    
-    let testData =  {
-    'name': 'Ericsson',
-    'ticker': 'ERIC',
-    'sector': 'Hardware',
-    'legacysector': 'IT',
-    'industry': 'Nails',
-    'legacyindustry': 'Software',
-    'marketcap': '$2,987,580',
-    'perf10yr': null,
-    'perfvssector10yr': null     
-  }
-    let testData2 =  {
-            'name': 'Majesco',
-            'ticker': 'MACO',
-            'sector': 'Industrials',
-            'legacysector': 'Books',
-            'industry': 'Chemical Manufacturing',
-            'legacyindustry': 'Paper',
-            'marketcap': '$1,187,580',
-            'perf10yr': null,
-            'perfvssector10yr': null     
-          }
-    let array = [];
-    array.push(testData);
-    array.push(testData2);
-    
-  return array;
-      
-//    let endpointUrl = 'https://2wdm1205e1.execute-api.us-east-1.amazonaws.com/DEV/companyInfo'
-//    return this.http.get(endpointUrl)
-//                    .toPromise()
-//                    .then(res => <CompanyOverview[]> res)
-//                      .then(data => {return data;});
+
+    let endpointUrl = 'https://2wdm1205e1.execute-api.us-east-1.amazonaws.com/DEV/companylist'
+    return this.http.get(endpointUrl)
+                    .toPromise()
+                    .then(res => <CompanyOverview[]> res)
+                      .then(data => {return data;});
   }
   
 
