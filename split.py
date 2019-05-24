c1a2d8d24abde218d1d018f01f231814ed09a248
root <root@ip-172-31-7-252.ec2.internal>
2019-05-24 20:37:28 +0000

    created split.py to split the data to be parallelized

---
+++ split.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def splitDF(n=16,inputFile='data.csv'):
+    import pandas as pd
+    import numpy as np
+
+    df = pd.read_csv(inputFile)
+    ar = np.array_split(df,3)
+    counter = 1
+    for item in ar:
+        dfTemp = pd.DataFrame(item)
+        dfTemp.to_csv('data{}_proc_input.csv'.format(counter))
+        counter += 1
+    
+    
