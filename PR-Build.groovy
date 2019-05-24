1e26674c1c533b8a0ef2f406c23def64844b69bb
stephencshelton <sshelton@sevatec.com>
2019-05-24 14:22:48 -0400

    Wrong options

--- PR-Build.groovy
+++ PR-Build.groovy
@@ -124,7 +124,7 @@ podTemplate(
           try {
             dir("${WORKSPACE}/mdas-client") {
               sh "npm install"
-              sh "ng test --no-sandbox"
+              sh "ng test --browsers Chrome_no_sandbox"
             }
             output('Test', 'success')
           }
@@ -144,7 +144,7 @@ podTemplate(
           try {
             dir("${WORKSPACE}/mdas-client") {
               sh "npm install"
-              sh "ng e2e --no-sandbox"
+              sh "ng e2e --browsers Chrome_no_sandbox"
             }
             output('Integration Tests', 'success')
           }
