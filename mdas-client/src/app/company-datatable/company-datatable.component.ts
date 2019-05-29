defc59e280d31ccc2ac3a3baad8c2cf3dec63fe4
Thomas Essig <tessig@harris.com>
2019-05-29 15:46:35 -0400

    Allow for manual filter of sector/industry in datatable
    also removed the performance widget from the company component
    to remove the error barrage

--- mdas-client/src/app/company-datatable/company-datatable.component.ts
+++ mdas-client/src/app/company-datatable/company-datatable.component.ts
@@ -19,12 +19,17 @@ export class CompanyDatatableComponent implements OnInit, OnDestroy {
   cols: any[];
   selectedCompany: CompanyOverview;
 
+  @ViewChildren('hiddenfilter')
+  hiddenfilters: QueryList<any>;
+  
   @ViewChildren('filter')
   filters: QueryList<any>;
   
-  sectorFilter: string = '';
-  previousIndustryFilter: string = '';
-  industryFilter: string = '';
+  hiddenSectorFilter: string = '';
+  previousHiddenIndustryFilter: string = '';
+  hiddenIndustryFilter: string = '';
+  sectorParam: string = '';
+  industryParam: string = '';
 
     sub: any;
 
@@ -44,9 +49,9 @@ export class CompanyDatatableComponent implements OnInit, OnDestroy {
                           // Defaults to 0 if no query param provided.
                           console.log('params');
                           console.log(params);
-                          this.previousIndustryFilter = this.industryFilter;
-                          this.sectorFilter = params['sector'];
-                          this.industryFilter = params['industry'];
+                          this.previousHiddenIndustryFilter = this.hiddenIndustryFilter;
+                          this.hiddenSectorFilter = this.sectorParam = params['sector'];
+                          this.hiddenIndustryFilter = this.industryParam = params['industry'];
                       });
       
     //initialize table
@@ -73,23 +78,54 @@ export class CompanyDatatableComponent implements OnInit, OnDestroy {
   }
   
   filter(){
-      if(this.filters != null && this.filters.toArray().length > 0){
-          this.filters.toArray()[1].nativeElement.value = this.sectorFilter;
-          this.filters.toArray()[2].nativeElement.value = this.industryFilter;
+      if(this.hiddenfilters != null && this.hiddenfilters.toArray().length > 0){
+          this.hiddenfilters.toArray()[1].nativeElement.value = this.hiddenSectorFilter;
+          this.hiddenfilters.toArray()[2].nativeElement.value = this.hiddenIndustryFilter;
 
 //        Industry filtering always yields a subset of a particular sector,
 //        so we only need to filter on industry if we have one
-          if(this.industryFilter || (this.previousIndustryFilter && !this.industryFilter)){
-              this.table.filter(this.industryFilter, 'atindustry', 'contains');
-          }
-          else{
-              this.table.filter(this.sectorFilter, 'atsector', 'contains');
-          }
+
+//          if(this.hiddenIndustryFilter || (this.previousHiddenIndustryFilter && !this.hiddenIndustryFilter)){
+//              this.table.filter(this.hiddenIndustryFilter, 'atindustry', 'contains');
+//          }
+//          else{
+//              this.table.filter(this.hiddenSectorFilter, 'atsector', 'contains');
+//          }
+          this.table.filter(this.hiddenSectorFilter, 'atsector', 'contains');
+          this.table.filter(this.hiddenIndustryFilter, 'atindustry', 'contains');
       }
   }
   
   ngAfterViewChecked(){
-      this.filter();
+      console.log('sector from url: ' + this.sectorParam);
+      console.log('industry from url: ' + this.industryParam);
+      
+      if(this.filters == null || this.filters.toArray().length == 0){
+          console.log('page still loading');
+          this.filter();
+      } else if(this.filters.toArray()[1].nativeElement.value ||
+                this.filters.toArray()[2].nativeElement.value){
+          console.log('user filters, use those')
+          
+          
+          this.hiddenSectorFilter = this.filters.toArray()[1].nativeElement.value;
+          this.hiddenIndustryFilter = this.filters.toArray()[2].nativeElement.value;
+          
+          console.log('hiddenSector: ' + this.hiddenSectorFilter);
+          console.log('hiddenIndustry: ' + this.hiddenIndustryFilter);
+          
+          this.filter();
+      } else {
+          console.log('no user filter, use params')
+          
+          this.hiddenIndustryFilter = this.industryParam;
+          this.hiddenSectorFilter = this.sectorParam;
+          
+          console.log('hiddenSector: ' + this.hiddenSectorFilter);
+          console.log('hiddenIndustry: ' + this.hiddenIndustryFilter);
+          
+          this.filter();
+      }
   }
   
   loadCompaniesLazy(event: LazyLoadEvent){
@@ -103,5 +139,9 @@ export class CompanyDatatableComponent implements OnInit, OnDestroy {
           }
       }, 250);
       
+  }
+  
+  clear(item: any){
+      console.log(item);
   }
 }
