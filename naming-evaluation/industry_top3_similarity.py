d456cb8812e52d3c18aac0fcd6cdb35ea62712ec
Owen Davies <odavies@peraton.com>
2019-05-30 23:42:41 +0000

    fixing up capitalization and spacing of sector/industry names

--- naming-evaluation/industry_top3_similarity.py
+++ naming-evaluation/industry_top3_similarity.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
             distances.sort()
             names = "{industry1} and {industry2}".format(
                     id=line[0], industry1=distances[0][1],
-                    industry2=distances[0][2])
+                    industry2=distances[0][2]).title()
             
             count = counts.get(names, 0)
             if count:
