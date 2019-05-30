987cb81f561cb221656f202a0cbf72b92ab22452
eugenechoGH <44753441+eugenechoGH@users.noreply.github.com>
2019-05-30 11:28:46 -0400

    wrapper does not run sector summary

--- wrapper.pl
+++ wrapper.pl
@@ -3,7 +3,6 @@
 `perl gen-industry-company-list.pl company_sector_industry.csv companylist.csv >> log.txt`;
 `perl count_sic_freq.pl Sector >> log.txt`;
 `perl count_sic_freq.pl Industry >> log.txt`;
-`perl sector-name-sic-bucket-summary.pl >> log.txt`;
 `perl Print_Sector_Industry_names.pl sec-sic-code-name.txt Sector 3 > sectors_top3_sic_words.csv`;
 `perl Print_Sector_Industry_names.pl sec-sic-code-name.txt Industry 3 > industries_top3_sic_words.csv`;
 `perl getSectorHighFreqSicNames.pl sec-sic-code-name.txt 3 > sector_name.csv`;
