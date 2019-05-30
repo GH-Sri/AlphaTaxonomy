cafe8c250583311f2c68a62000173da3031256fc
Owen Davies <odavies@peraton.com>
2019-05-30 06:25:01 +0000

    Autocommit for all these query-only functions.  Also fix the broken environment syntax and import the needed library.

--- lambda-json-rest/functions/get-companylist/lambda_function.py
+++ lambda-json-rest/functions/get-companylist/lambda_function.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import logging
 import rds_config
@@ -7,10 +8,10 @@ from psycopg2.extras import RealDictCursor
 import json
 
 # rds settings
-rds_host  = return os.environ['rds_host'] or rds_config.rds_host
-name = return os.environ['db_username'] or rds_config.db_username
-password = return os.environ['db_password'] or rds_config.db_password
-db_name = return os.environ['db_name'] or rds_config.db_name
+rds_host = os.getenv('rds_host') or rds_config.rds_host
+name = os.getenv('db_username') or rds_config.db_username
+password = os.getenv('db_password') or rds_config.db_password
+db_name = os.getenv('db_name') or rds_config.db_name
 
 # logging
 logger = logging.getLogger()
@@ -19,6 +20,7 @@ logger.setLevel(logging.INFO)
 # connect using creds from rds_config.py
 try:
     conn = psycopg2.connect(host=rds_host,user=name,password=password,dbname=db_name)
+    conn.autocommit = True
 except:
     logger.error("ERROR: Unexpected error: Could not connect to PostgreSQL instance.")
     sys.exit()
@@ -57,10 +59,11 @@ LIMIT {}
 
 # executes upon API event
 def lambda_handler(event, context):
-    logger.info('event is' + json.dumps(event))
 
+    # First blank dict is default if event does not contain 'queryStringParameters'
+    # Second blank dict is default if event has 'queryStringParameters' but value is null
     qsp = event.get('queryStringParameters',{}) or {}
-    logger.info('qsp is' + json.dumps(qsp))
+
     # Apply filters for Sector or Industry if passed on query string
     if 'sector' in qsp: 
         swc = " AND COALESCE(sname.name, 'Sector ' || csi.sector) = '{}'".format(qsp.get('sector')) 
@@ -81,9 +84,7 @@ def lambda_handler(event, context):
     with conn.cursor(cursor_factory=RealDictCursor) as cur:
         query=template.format(swc,iwc,order,direction,offset,limit)
         logger.info("About to execute " + query)
-        conn.rollback()
         cur.execute(query)
-        conn.commit()
         return {
             'statusCode': 200,
             'headers': { 'Content-Type': 'application/json',
--- lambda-json-rest/functions/get-competitorinfo-by-company/lambda_function.py
+++ lambda-json-rest/functions/get-competitorinfo-by-company/lambda_function.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import logging
 import rds_config
@@ -7,10 +8,10 @@ from psycopg2.extras import RealDictCursor
 import json
 
 # rds settings
-rds_host  = return os.environ['rds_host'] or rds_config.rds_host
-name = return os.environ['db_username'] or rds_config.db_username
-password = return os.environ['db_password'] or rds_config.db_password
-db_name = return os.environ['db_name'] or rds_config.db_name
+rds_host = os.getenv('rds_host') or rds_config.rds_host
+name = os.getenv('db_username') or rds_config.db_username
+password = os.getenv('db_password') or rds_config.db_password
+db_name = os.getenv('db_name') or rds_config.db_name
 
 # logging
 logger = logging.getLogger()
@@ -19,6 +20,7 @@ logger.setLevel(logging.INFO)
 # connect using creds from rds_config.py
 try:
     conn = psycopg2.connect( host=rds_host,user=name,password=password,dbname=db_name)
+    conn.autocommit = True
 except:
     logger.error("ERROR: Unexpected error: Could not connect to PostgreSQL instance.")
     sys.exit()
@@ -50,7 +52,6 @@ def lambda_handler(event, context):
         query=template.format(company)
         logger.info("About to execute " + query)
         cur.execute(query)
-        conn.commit()
         return {
             'statusCode': 200,
             'headers': { 'Content-Type': 'application/json',
